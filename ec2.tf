c7ecca4a5620b2f4ca40b2d0f8001faf7de5ddb2
sayali pawar <114933525+sayalipawar1295@users.noreply.github.com>
2026-04-02 04:26:27 -0700

    Refactor EC2 instance and security group configuration
    Fixed typos and improved formatting in security group and instance configuration.

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "server" {
-  ami           = "ami-045443a70fafb8bbc"
-  instance_type = "t3.micro"
-  key_name      = "oncdec-b36"
-  vpc_security_group_ids = [aws_security_grp.sg.id]
-  user_data = <<EOF
+  ami                    = "ami-045443a70fafb8bbc"
+  instance_type          = "t3.micro"
+  key_name               = "oncdec-b36"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  user_data              = <<EOF
     #!/bin/bash
     sudo -i
-    yum install httpd
+    yum install httpd -y
     systemctl start httpd
     systemctl enable httpd
-    echo "hello terrafom" > /var/www/htmlinsex.html
+    echo "hello terrafom" > /var/www/html/index.html
     EOF
 
   tags = {
@@ -17,30 +17,35 @@ resource "aws_instance" "server" {
   }
 }
 
-resource "aws_security_grp" "sg" {
-    vpc_id = "vpc-0e63dbcf6cd169d82" 
+resource "aws_security_group" "sg" {
+  vpc_id = "vpc-0e63dbcf6cd169d82"
 
-    ingress {
-        from_port =22
-        to_port = 22
-        protocol "tcp"
-        cidr_block = ["0.0.0.0/0"]
-
-    ingress {
-        from_port =80
-        to_port = 80
-        protocol "tcp"
-        cidr_block = ["0.0.0.0/0"]
-    }
-
-    engrss {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_block = ["0.0.0.0/0"]
-        }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+}
 
 
+
+
+
+  }
 }
