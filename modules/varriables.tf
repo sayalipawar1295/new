9ee37b441368eb4b2aa8659dc232e488322fd8be
sayali pawar <114933525+sayalipawar1295@users.noreply.github.com>
2026-04-09 03:41:11 -0700

    Add variables for VPC and subnet configuration

---
+++ modules/varriables.tf
@@ -0,0 +1,39 @@
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "cidr block for vpc"
+
+}
+
+
+variable "subnet_cidr_block" {
+  type        = string
+  description = "cidr block for subnet"
+
+}
+
+variable "az" {
+  type = string
+
+}
+
+variable "assign_pub_ip" {
+  type = bool
+
+}
+
+
+variable "ami_id" {
+  type = string
+
+}
+
+variable "ins_type" {
+  type = string
+
+}
+
+variable "key_pair" {
+  type = string
+
+}
